e06460dd38fe0439e667b243266670865a5f61af
mmy <mmy_xize@163.com>
2019-08-22 00:01:04 +0800

    安装测试插件，声明所需要的函数

---
+++ __tests__/main-spec.js
@@ -0,0 +1,5 @@
+const printReciept = require('../main');
+
+it ('should add two numbers', () => {
+    expect(printReciept(2, 3)).toBe(5);
+});
---
+++ main.js
@@ -0,0 +1,50 @@
+'use strict'
+
+function printReciept(barcodes) {
+    var goodsInfo = queryGoodsDB(barcodes);
+    var cartResult = checkoutCart(goodsInfo);
+    var receipt = transformFormat(cartResult);
+    return receipt;
+}
+
+function queryGoodsDB(barcodes) {
+    const goodsDataBase = [
+        {"id": "0001", "name" : "Coca Cola", "price": 3},
+        {"id": "0002", "name" : "Diet Coke", "price": 4},
+        {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+        {"id": "0004", "name" : "Mountain Dew", "price": 6},
+        {"id": "0005", "name" : "Dr Pepper", "price": 7},
+        {"id": "0006", "name" : "Sprite", "price": 8},
+        {"id": "0007", "name" : "Diet Pepsi", "price": 9},
+        {"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
+        {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
+        {"id": "0010", "name" : "Fanta", "price": 12}
+    ];
+
+    if (barcodes !== null) {
+        for (let i = 0; i < barcodes.length; i++) {
+            const singleGoodsInfo = {};
+            for (let j = 0; j < goodsDataBase.length; j++) {
+                if (barcodes[i] === goodsDataBase[j].id) {
+                    singleGoodsInfo.name = goodsDataBase[j].name;
+                    singleGoodsInfo.price = goodsDataBase[j].price;
+                    // TODO
+                }
+                
+                
+            }
+            // TODO
+            
+        }
+    }
+}
+
+function checkoutCart(goodsInfo) {
+    // TODO
+}
+
+function transformFormat(cartResult) {
+    // TODO
+}
+
+module.exports = printReciept;
