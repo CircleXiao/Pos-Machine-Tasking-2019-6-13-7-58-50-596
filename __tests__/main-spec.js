334b2ac9461d588b616cdf57ca25e826b8f27689
mmy <mmy_xize@163.com>
2019-08-22 12:04:19 +0800

    version2.0 内容更改在main.js，修改测试用例

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,5 @@
 const printReciept = require('../main');
 
-it ('should add two numbers', () => {
-    expect(printReciept(2, 3)).toBe(5);
+it ('should settle the cart from barcodes to receipt', () => {
+    expect(printReciept(['0001', '0003', '0005', '0003'])).toBe('Receipts\n------------------------------------------------\nCoca Cola\t\t\t3\t\t1\nPepsi-Cola\t\t\t5\t\t2\nDr Pepper\t\t\t7\t\t1\n\n------------------------------------------------\nPrice：20');
 });
--- main.js
+++ main.js
@@ -1,50 +1,84 @@
 'use strict'
 
+const goodsDataBase = [
+    {"id": "0001", "name" : "Coca Cola", "price": 3},
+    {"id": "0002", "name" : "Diet Coke", "price": 4},
+    {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+    {"id": "0004", "name" : "Mountain Dew", "price": 6},
+    {"id": "0005", "name" : "Dr Pepper", "price": 7},
+    {"id": "0006", "name" : "Sprite", "price": 8},
+    {"id": "0007", "name" : "Diet Pepsi", "price": 9},
+    {"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
+    {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
+    {"id": "0010", "name" : "Fanta", "price": 12}
+];
+
 function printReciept(barcodes) {
-    var goodsInfo = queryGoodsDB(barcodes);
-    var cartResult = checkoutCart(goodsInfo);
-    var receipt = transformFormat(cartResult);
+    let itemsMap = countItems(barcodes);
+    let cartGoodsInfo = fillItems(itemsMap);
+    let totalMoney = calcuTotal(cartGoodsInfo);
+    let receipt = createRecipt(cartGoodsInfo, totalMoney);
     return receipt;
 }
 
-function queryGoodsDB(barcodes) {
-    const goodsDataBase = [
-        {"id": "0001", "name" : "Coca Cola", "price": 3},
-        {"id": "0002", "name" : "Diet Coke", "price": 4},
-        {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
-        {"id": "0004", "name" : "Mountain Dew", "price": 6},
-        {"id": "0005", "name" : "Dr Pepper", "price": 7},
-        {"id": "0006", "name" : "Sprite", "price": 8},
-        {"id": "0007", "name" : "Diet Pepsi", "price": 9},
-        {"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
-        {"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
-        {"id": "0010", "name" : "Fanta", "price": 12}
-    ];
-
-    if (barcodes !== null) {
-        for (let i = 0; i < barcodes.length; i++) {
-            const singleGoodsInfo = {};
-            for (let j = 0; j < goodsDataBase.length; j++) {
-                if (barcodes[i] === goodsDataBase[j].id) {
-                    singleGoodsInfo.name = goodsDataBase[j].name;
-                    singleGoodsInfo.price = goodsDataBase[j].price;
-                    // TODO
-                }
-                
-                
-            }
-            // TODO
-            
-        }
+function countItems(items) {
+    let itemsMap = new Map();
+    for (let i = 0; i < items.length; i++) {
+        if (itemsMap.get(items[i]) == null) {
+            itemsMap.set(items[i], 1);
+        } else {
+            itemsMap.set(items[i], itemsMap.get(items[i]) + 1);
+        }       
     }
+    return itemsMap;
+    
 }
 
-function checkoutCart(goodsInfo) {
-    // TODO
+function fillItems(itemsMap) {
+    let cartGoodsInfo = [];
+
+    itemsMap.forEach(function (key, value) {
+        cartGoodsInfo.push({id: value, count: key});
+    });
+
+    for (let i = 0; i < cartGoodsInfo.length; i++) {
+        for (let j = 0; j < goodsDataBase.length; j++) {
+            if (cartGoodsInfo[i].id === goodsDataBase[j].id) {
+                cartGoodsInfo[i] = {
+                    name: goodsDataBase[j].name,
+                    price: goodsDataBase[j].price,
+                    count: cartGoodsInfo[i].count
+                };
+            }           
+        }       
+    }
+    return cartGoodsInfo; 
 }
 
-function transformFormat(cartResult) {
-    // TODO
+function calcuTotal(cartGoodsInfo) {
+    let totalMoney = 0;
+    for (let i = 0; i < cartGoodsInfo.length; i++) {
+        totalMoney += cartGoodsInfo[i].price * cartGoodsInfo[i].count;       
+    }
+    return totalMoney;   
+}
+
+function createRecipt(cartGoodsInfo, totalMoney) {
+    let separator = '\n------------------------------------------------\n';
+    let receiptTitle = 'Receipts' + separator;
+    let receiptBody = createReciptBody(cartGoodsInfo);
+    let receiptFooter = separator + 'Price：' + totalMoney; 
+    let receipt = receiptTitle + receiptBody + receiptFooter;
+    return receipt;
+}
+
+function createReciptBody(cartGoodsInfo) {
+    let receiptBody = '';
+    for (let i = 0; i < cartGoodsInfo.length; i++) {
+        receiptBody += cartGoodsInfo[i].name + '\t\t\t' + cartGoodsInfo[i].price + '\t\t' + cartGoodsInfo[i].count + '\n';     
+    }
+    return receiptBody;
 }
 
 module.exports = printReciept;
+
